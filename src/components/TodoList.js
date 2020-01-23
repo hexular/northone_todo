9518181c05fe32686e32fdcafa60020d511b95fa
Dan <hexular@gmail.com>
2020-01-23 11:24:25 -0500

    add styling to list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import './Todo.css'
 import IconButton from '@material-ui/core/IconButton'
-// import DeleteIcon from 
+import DeleteIcon from '@material-ui/icons/Delete'
+import CheckIcon from '@material-ui/icons/Check'
 
 export default function List(props) {
 
   const list = props.todos.map((todo, i) => {
     return !todo.deleted && (
       <React.Fragment>
-      <li 
-        className={todo.done ? "done" : "pending"} 
-        onClick={() => props.markDone(i)} 
-        key={i}
-      >
-        {todo.name}
-        {/* {todo.due} */}
-        <IconButton onClick={() => props.deleteTodo(i)}>
-          {/* <DeleteIcon /> */}
-          delete
-        </IconButton>
-      </li>
+        <li className={todo.done ? "done list" : "pending list"} key={i}>
+          <p className="title">{todo.name}</p>
+          {/* {todo.due} */}
+          <div className="icons">
+            <IconButton onClick={() => props.markDone(i)}>
+              <CheckIcon />
+            </IconButton>
+            <IconButton onClick={() => props.deleteTodo(i)}>
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        </li>
       </React.Fragment>
     )
   })
