537ab95d841fb4013d03220ff68c78212eb01b92
Dan <hexular@gmail.com>
2020-01-23 13:27:09 -0500

    add material ui styling to form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import './App.css'
 import Input from './components/Input'
 import TodoList from './components/TodoList'
+import IconButton from '@material-ui/core/IconButton'
+import AddIcon from '@material-ui/icons/Add'
 
 const today = new Date().toISOString().slice(0, 10)
 
 function App() {
 
+  const [add, setAdd] = useState(false)
   const [todos, setTodos] = useState([
     {
       name: "Build To Do App", 
@@ -46,16 +49,21 @@ function App() {
     setTodos(newTodos)
   }
 
+  const cancelForm = () => {
+    setAdd(false)
+  }
+
   const newTodo = (name, desc, category, date) => {
     const newTodos = [...todos, {name: name, desc: desc, category: category, date: date, done: false, deleted: false}]
     setTodos(newTodos)
+    setAdd(false)
   }
 
   return (
     <div className="App">
       <h2>Todo List</h2>
       <TodoList todos={todos} markDone={markDone} deleteTodo={deleteTodo}/>
-      <Input newTodo={newTodo} today={today}/>
+      {add ? <Input newTodo={newTodo} today={today} cancelForm={cancelForm}/> : <IconButton><AddIcon onClick={() => setAdd(true)}/></IconButton>}
     </div>
   )
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react'
+import './Input.css'
+import AddIcon from '@material-ui/icons/Add'
+import IconButton from '@material-ui/core/IconButton'
+import ClearIcon from '@material-ui/icons/Clear'
+import TextField from '@material-ui/core/TextField'
 
 export default function Input(props){
 
@@ -7,8 +12,7 @@ export default function Input(props){
   const [category, setCategory] = useState('')
   const [date, setDate] = useState(props.today)
 
-  const onSubmit = event => {
-    event.preventDefault()
+  const onSubmit = () => {
     props.newTodo(name, desc, category, date)
     setName('')
     setDesc('')
@@ -17,38 +21,54 @@ export default function Input(props){
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <input 
+    <form className="form" autoComplete="off">
+      <TextField 
         value={name} 
         type="text" 
         placeholder="Add New Todo" 
         id="name" 
-        onChange={event => setName(event.target.value)}>
-      </input>
-      <input 
+        onChange={event => setName(event.target.value)} 
+        className="input"
+        variant="outlined"
+      />
+      
+      <TextField 
         value={desc} 
         type="text" 
         placeholder="Description" 
         id="desc"
-        onChange={event => setDesc(event.target.value)}>
-      </input>
-      <input 
+        onChange={event => setDesc(event.target.value)} 
+        className="input"
+        variant="outlined"
+      />
+      
+      <TextField 
         value={category} 
         type="text" 
         placeholder="Select Category" 
         id="category"
-        onChange={event => setCategory(event.target.value)}>
-      </input>
-      <input 
+        onChange={event => setCategory(event.target.value)} 
+        className="input" 
+        variant="outlined"
+      />
+      
+      <TextField 
         type="date" 
         id="date" 
         name="due-date"
         min={props.today} max="2020-12-31"
-        value={date}
-        onChange={event => setDate(event.target.value)}>
-      </input>
-      <input type="submit" value="Add Todo">
-      </input>
+        defaultValue={date}
+        onChange={event => setDate(event.target.value)} 
+        variant="outlined"
+      />
+      <div className="icons">
+        <IconButton>
+          <AddIcon onClick={() => onSubmit()}/>
+        </IconButton>
+        <IconButton onClick={() => props.cancelForm()}>
+          <ClearIcon/>
+        </IconButton>
+      </div>
     </form>
   )
 }
